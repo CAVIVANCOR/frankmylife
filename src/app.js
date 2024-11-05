090b143629a94b791cf92ccfd07e3cf6042b38d6
CARLOS ALBERTO VIVANCO RODRIGUEZ <SISTEMAS.CAVR@GMAIL.COM>
2024-11-05 17:48:28 -0500

    A Produccion

--- src/app.js
+++ src/app.js
@@ -2,24 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const mainRoute = require('./routes/index.js');
 const {responseError} = require('./utils/index.js');
-
-
+const cors = require('cors');
 const multer = require('multer');
-
-
+const path = require('path'); // Importar path
 const app = express();
-
 app.use(morgan('dev'));
 app.use(express.json());
-
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","http://localhost:5173");
-    res.header("Access-Control-Allow-Credentials",true);
-    res.header("Access-Control-Allow-Methods","GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
-});
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'HEAD', 'OPTIONS', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'Accept', 'X-Requested-With', 'Content-Type', 'Authorization', 'Access-Control-Request-Method', 'Access-Control-Request-Headers']
+}));
 // Configurar el middleware de multer para manejar la carga de archivos
 const storageFotos = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -35,8 +29,6 @@ app.post('/media/fotos', uploadFotos.single('file'), (req, res) => {
     res.status(200).json({ message: 'Foto subido exitosamente' });
 });
 app.use('/media/fotos',express.static('media/fotos'));
-
-
 const storageVideos = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'media/videos'); // Directorio donde se guardarán los archivos
@@ -51,10 +43,7 @@ app.post('/media/videos', uploadVideos.single('file'), (req, res) => {
     res.status(200).json({ message: 'Video subido exitosamente' });
 });
 app.use('/media/videos',express.static('media/videos'));
-
-
 app.use('/media/audios',express.static('media/audios'));
-
 // Configurar el middleware de multer para manejar la carga de archivos
 const storageUsuarios = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -70,12 +59,23 @@ app.post('/media/usuarios', uploadUsuarios.single('file'), (req, res) => {
     res.status(200).json({ message: 'Archivo subido exitosamente' });
 });
 app.use('/media/usuarios',express.static('media/usuarios'));
-
 app.use('/media/sistema',express.static('media/sistema'));
+
+
+// Servir archivos estáticos del frontend en producción
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+// Redirigir todas las rutas a index.html para manejar el enrutamiento del frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend/dist', 'index.html'));
+});
+
+
+
+
 
 app.use(mainRoute);
 app.use((err,req,res,next)=>{
     responseError(res,err.statusCode,err.message,err.nameTable);
 });
-
 module.exports = app;
--- src/db.js
+++ src/db.js
@@ -10,31 +10,23 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
         timezone: 'America/Lima'
     }
 });
-
 const AccesoModel = require('./models/usuarios/Acceso');
 const RolModel = require('./models/usuarios/Rol');
 const UsuarioModel = require('./models/usuarios/Usuario');
 const MediaModel = require('./models/media/Media');
 const CategoriesModel = require('./models/media/Categories');
-
 UsuarioModel(sequelize);
 RolModel(sequelize);
 AccesoModel(sequelize);
 MediaModel(sequelize);
 CategoriesModel(sequelize);
-
 const {Acceso, Rol, Usuario, Media, Categories} = sequelize.models;
-
 Rol.hasMany(Usuario);
 Usuario.belongsTo(Rol);
-
 Usuario.hasMany(Acceso);
 Acceso.belongsTo(Usuario);
-
 Usuario.hasMany(Media);
 Media.belongsTo(Usuario);
-
 Categories.hasMany(Media);
 Media.belongsTo(Categories);
-
 module.exports={sequelize, SERVER_PORT, ...sequelize.models};
